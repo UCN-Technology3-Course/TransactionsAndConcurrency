7e3e19e5d061d8e88f3014f369576766b2abbb18
Lars Nysom <lars.nysom@gmail.com>
2020-09-16 13:32:04 +0200

    Add project files.

---
+++ ZooModel/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ZooModel
+{
+    public partial class Animal : ICloneable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? LastUpdate { get; set; }
+        public Species Species { get; set; }
+
+        public object Clone()
+        {
+            return new Animal
+            {
+                Id = Id,
+                Name = Name,
+                LastUpdate = LastUpdate,
+                Species = Species
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Species?.Name}";
+        }
+
+        
+    }
+}
